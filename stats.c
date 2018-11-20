a3e452e0ba6af123594833711040ebe375d34e93
Nguyen Van Hoa Vu <bigboy.061293@gmail.com>
2018-11-20 00:05:36 -0800

    update stats.c in dataset -> set to 40

--- stats.c
+++ stats.c
@@ -31,17 +31,17 @@
 #include "stats.h"
 
 /* Size of the Data Set */
-#define SIZE (10)
+#define SIZE (40)
 
 void main() {
 
-/*   unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
+   unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
                               114, 88,   45,  76, 123,  87,  25,  23,
                               200, 122, 150, 90,   92,  87, 177, 244,
                               201,   6,  12,  60,   8,   2,   5,  67,
-                                7,  87, 250, 230,  99,   3, 100,  90}; */
+                                7,  87, 250, 230,  99,   3, 100,  90}; 
 								
-  unsigned char test[SIZE] = { 12, 1, 34,12, 12,22,14,3,45,99};
+//  unsigned char test[SIZE] = { 12, 1, 34,12, 12,22,14,3,45,99};
   /* Other Variable Declarations Go Here */
   /* Statistics and Printing Functions Go Here */
 	printf("The original data set: \n");
